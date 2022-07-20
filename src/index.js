897bd88df9662cbf399fc8e90dd7f18cd7cb5ec8
rokas <rokasra@gmail.com>
2022-07-20 11:36:19 +0300

    ready for ts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import './style/reset.css';
 import './style/style.css';
-
-// import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-
-render(<App />, document.getElementById('app'));
---
+++ src/indexReact.js
@@ -0,0 +1,8 @@
+import './style/reset.css';
+import './style/style.css';
+
+// import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('app'));
